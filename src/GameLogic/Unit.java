dab9593bcdd65c2a0954e9a50d7f7fc27b430534
awangg <andy_wang@student.wayland.k12.ma.us>
2017-09-13 08:15:49 -0400

    Unit finished Vec2d added 'add' function

--- src/GameLogic/Unit.java
+++ src/GameLogic/Unit.java
@@ -3,7 +3,20 @@ import Utilities.*;
 
 public class Unit {
     String name;
-    Vec2d currentpos;
+    Vec2d currentpos, displacement;
     int cost;
 
+    Unit(String n, Vec2d pos, int c) {
+        name = n;
+        cost = c;
+        currentpos = pos;
+    }
+
+    void move() {
+        currentpos.add(displacement);
+    }
+
+    void turn(Vec2d dir) {
+        displacement = dir;
+    }
 }
--- src/Utilities/Vec2d.java
+++ src/Utilities/Vec2d.java
@@ -3,8 +3,13 @@ package Utilities;
 public class Vec2d {
     public int x;
     public int y;
-    Vec2d(int xx, int yy) {
-        x = xx;
-        y = yy;
+    Vec2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void add(Vec2d other) {
+        x += other.x;
+        y += other.y;
     }
 }
