dbfbfcab37db3228061d69f4fc1bdd35ca344210
awangg <andy_wang@student.wayland.k12.ma.us>
2017-09-15 09:57:21 -0400

    added Map class to logic

---
+++ src/game/logic/Map.java
@@ -0,0 +1,70 @@
+package game.logic;
+
+import utilities.Vec2d;
+
+public class Map {
+    Tiles tiles;
+    Tiles[][] mapArray;
+    Tiles[][] viewableMapArray;
+
+    public Vec2d startTile;
+    Vec2d stopTile;
+    int x;
+    int y;
+    int mapSize = 100;
+
+    public Map(int width, int height, Vec2d x, Vec2d y) {
+        Tiles[][] mapArray = new Tiles[width][height];
+        Tiles[][] viewableMapArray = new Tiles[width][height];
+
+        for (int row = 0; row < width; row++) {
+            for (int col = 0; col < height; col++) {
+                viewableMapArray[row][col] = Tiles.UNKNOWN;
+            }
+        }
+        startTile = x;
+        stopTile = y;
+
+        this.x = width;
+        this.y = height;
+
+    }
+
+    public Tiles[][] returner() {
+
+
+
+//        for (int row = 0; row < x; row++) {
+//            for (int col = 0; col < y; col++) {
+//                if (viewableMapArray[row][col] != Tiles.UNKNOWN){//true means that you can see through the cloud
+//                    tilesArray[row][col] = mapArray[row][col];
+//                } else {
+//                    tilesArray[row][col] = Tiles.UNKNOWN;
+//                }
+//            }
+//        }
+
+
+//        tilesArray[startTile.x][startTile.y] = mapArray[startTile.x][startTile.y];
+//        tilesArray[stopTile.x][stopTile.y] = mapArray[stopTile.x][stopTile.y];
+//
+//
+//        return tilesArray;
+        return viewableMapArray;
+    }
+
+    public void update(Mission x){
+
+
+    }
+
+
+
+
+    public enum Tiles {
+        PATH, BLOCK, UNKNOWN, START, STOP
+    }
+
+
+
+}
--- src/game/logic/World.java
+++ src/game/logic/World.java
@@ -1,4 +1,5 @@
 package game.logic;
 
 public class World {
+
 }
