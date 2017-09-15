cbe9fab9566e7e7f4723d9b3b108fd09ff6c487d
haydnd <haydn_davies@student.wayland.k12.ma.us>
2017-09-15 10:20:24 -0400

    finished action class

--- src/Action.java
+++ src/Action.java
@@ -2,10 +2,24 @@
  * Created by student on 9/13/17.
  */
 public class Action {
-    private int Bot1;
-//kan
-    public Action(){
+
+    private ActionType actionType;
+    private int argument;
+    public enum ActionType{
+        TURN, MOVE
+    }
+
+    public Action(ActionType actionType, int argument ){
+        this.actionType = actionType;
+        this.argument = argument;
 
     }
 
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public int getArgument() {
+        return argument;
+    }
 }
